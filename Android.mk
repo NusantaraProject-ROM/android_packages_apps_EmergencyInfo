ed60dfc171aa8b565c88b54a27d54b1271374bde
Anton Hansson <hansson@google.com>
2018-02-22 16:11:50 +0000

    packages/apps/EmergencyInfo: Set LOCAL_SDK_VERSION where possible.
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Test: make relevant packages
    Bug: 73535841
    Change-Id: I0d217c7a39abd268e6027e75889676e50615b9c1

--- Android.mk
+++ Android.mk
@@ -17,6 +17,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_PACKAGE_NAME := EmergencyInfo
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_MODULE_TAGS := eng
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
