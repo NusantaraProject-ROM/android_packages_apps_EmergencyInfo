a1781d52ef94c8a60355580b15a02fff7304193c
Wesley.CW Wang <wesleycwwang@google.com>
2018-09-14 21:10:58 +0800

    Fix RunEmergencyInfoRoboTests Failures
    -Replace TestConfig with robolectric.properties.
    
    Test: make RunEmergencyInfoRoboTests
    
    Bug: 112168722
    Change-Id: If8cf923e42a73ad8b2317727c29b0b199b72b620
    Merged-In: If8cf923e42a73ad8b2317727c29b0b199b72b620

--- tests/robolectric/src/com/android/emergency/EmergencySearchIndexablesProviderTest.java
+++ tests/robolectric/src/com/android/emergency/EmergencySearchIndexablesProviderTest.java
@@ -21,7 +21,6 @@ import android.database.Cursor;
 import android.provider.SearchIndexablesContract;
 
 import com.android.emergency.R;
-import com.android.emergency.TestConfig;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -30,11 +29,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 
 /** Unit tests for {@link EmergencySearchIndexablesProvider}. */
 @RunWith(RobolectricTestRunner.class)
-@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION)
 public final class EmergencySearchIndexablesProviderTest {
     private EmergencySearchIndexablesProvider mProvider;
 
--- tests/robolectric/src/com/android/emergency/preferences/EmergencyContactsPreferenceTest.java
+++ tests/robolectric/src/com/android/emergency/preferences/EmergencyContactsPreferenceTest.java
@@ -36,7 +36,6 @@ import android.support.v7.preference.PreferenceScreen;
 import com.android.emergency.ContactTestUtils;
 import com.android.emergency.EmergencyContactManager;
 import com.android.emergency.PreferenceKeys;
-import com.android.emergency.TestConfig;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,11 +47,9 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
-import org.robolectric.annotation.Config;
 
 /** Unit tests for {@link EmergencyContactsPreference}. */
 @RunWith(RobolectricTestRunner.class)
-@Config(manifest = TestConfig.MANIFEST_PATH, sdk = TestConfig.SDK_VERSION)
 public class EmergencyContactsPreferenceTest {
     @Mock private PackageManager mPackageManager;
     @Mock private PreferenceManager mPreferenceManager;
